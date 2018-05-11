a755e00f288f53a2426804941cf0eddbcb5f0725
zac <zac@compy>
2018-05-11 15:15:45 -0400

    Fixed some things - should be good now

--- workspace/pebblegame/src/pebblegame/PebbleGraph.java
+++ workspace/pebblegame/src/pebblegame/PebbleGraph.java
@@ -5,73 +5,85 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PebbleGraph {
   
   private Map<Integer, Vertex> vertices = new HashMap<Integer,Vertex>();
   private int unassignedPebbles;
   private ArrayList<Vertex> searched = new ArrayList<Vertex>();
+  private ArrayList<String> strings = new ArrayList<String>();
 
   public PebbleGraph(String filename) {
     try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
       String str;
       while((str = br.readLine()) != null) {
-        String[] split = str.split(",");
-        Integer int0 = new Integer(split[0]);
-        Integer int1 = new Integer(split[1]);
-        Vertex v0 = vertices.get(int0);
-        if(v0 == null) {
-          v0 = new Vertex(int0);
-          vertices.put(int0, v0);
-          this.unassignedPebbles += 2;
-        }
-        Vertex v1 = vertices.get(int1);
-        if(v1 == null) {
-          v1 = new Vertex(int1);
-          vertices.put(int1, v1);
-          this.unassignedPebbles += 2;
-        }
-        if(v0.addChild(v1)) {
-          --this.unassignedPebbles;
-        }
-        else {
-          //if(findPebble(v0)) {
-          //  v0.addChild(v1);
-          //  --this.unassignedPebbles;
-          //}
-          if(collectPebbles(v0,v1)) {
-            v0.addChild(v1);
-            --this.unassignedPebbles;
-          }
-          else {
-            v0.addBadChild(v1);
-            System.out.println("Rigid Cluster Detected " + int0 + " " + int1);
-            //break;
-          }
-        }
+    	  strings.add(str);
       }
     } catch(IOException ioe) {
       System.out.println("An IOException occurred. Don't be dumb.");
       ioe.printStackTrace();
-    }   
+    }  
+    Collections.shuffle(strings);
+    for(String str : strings) {
+    	System.out.println(str);
+    }
+    for(String str : strings) {
+      String[] split = str.split(",");
+      Integer int0 = new Integer(split[0]);
+      Integer int1 = new Integer(split[1]);
+      Vertex v0 = vertices.get(int0);
+      if(v0 == null) {
+        v0 = new Vertex(int0);
+        vertices.put(int0, v0);
+        this.unassignedPebbles += 2;
+      }
+      Vertex v1 = vertices.get(int1);
+      if(v1 == null) {
+        v1 = new Vertex(int1);
+        vertices.put(int1, v1);
+        this.unassignedPebbles += 2;
+      }
+      if(v0.addChild(v1)) {
+        --this.unassignedPebbles;
+      }
+      else {
+        //if(findPebble(v0)) {
+        //  v0.addChild(v1);
+        //  --this.unassignedPebbles;
+        //}
+        if(collectPebbles(v0,v1)) {
+          v0.addChild(v1);
+          --this.unassignedPebbles;
+        }
+        else {
+          v0.addBadChild(v1);
+          System.out.println("Rigid Cluster Detected " + int0 + " " + int1);
+          //break;
+        }
+      }
+    }
+    
     if(unassignedPebbles == 3) {
       System.out.println("GOOD");
     } 
     else if(unassignedPebbles < 3) {
-      System.out.println("OVER");
+      System.out.println("OVER " + unassignedPebbles);
     } 
     else if(unassignedPebbles > 3) {
-      System.out.println("UNDER");
+      System.out.println("UNDER " + unassignedPebbles);
     } 
   }
 
   private boolean collectPebbles(Vertex v0, Vertex v1) {
-    while(!v0.hasTwoPebbles()) {
-      if(!this.findPebble(v0)) {
-        return false;
-      }
+	  while(!v0.hasTwoPebbles()) {
+		  searched.add(v1);
+		  if(!this.findPebble(v0)) {
+			  return false;
+		  }
     }
     while(!v1.hasTwoPebbles()) {
+    	searched.add(v0);
       if(!this.findPebble(v1)) {
         return false;
       }
@@ -80,7 +92,7 @@ public class PebbleGraph {
   }
 
   private boolean findPebble(Vertex vertex) {
-    searched.add(vertex);	
+    searched.add(vertex);
     ArrayList<Vertex> children = vertex.getChildren();
     for(Vertex v : children) {
       if(searched.contains(v)) {
@@ -94,9 +106,14 @@ public class PebbleGraph {
         return true;
       }
       else {
-        return findPebble(v);
+        //return findPebble(v);
+        if(findPebble(v)) {
+        	searched.clear();
+        	return true;
+        }
       }
     }
+    searched.clear();
     return false;
   }
   
